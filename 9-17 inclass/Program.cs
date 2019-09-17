90f8fa45b0e50fd407a17bce0a632f648a6fe5e2
schu8762 <J.Schuller@ou.edu>
2019-09-17 11:10:44 -0500

    participation string info and loop practice

--- 9-17 inclass/Program.cs
+++ 9-17 inclass/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//@author J-Schuller
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,26 @@ namespace _9_17_inclass
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your favorite baseball team?");
+            string answer = Console.ReadLine().ToUpper().Trim(); // Trim takes the spaces out of it
+            string nextAnswer = "Somethine else";
+            string answerUppercase = answer.ToUpper();
+
+
+            for (int i = 0; i < answerUppercase.Length; i++)
+            {
+                Console.Write(answerUppercase[i] + " ");
+            }
+
+            Console.WriteLine($"Your favorite team has {answer.Length} characters in it");
+
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
